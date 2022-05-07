bfb4b8775e45aa69db2e1940d6171e618716cae4
Karishma Ghiya <kaghiya@microsoft.com>
2022-05-06 18:01:23 -0700

    update the files

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+import * as dotenv from "dotenv";
+import {ClientCertificateCredential} from "@azure/identity";
+dotenv.config();
+
+
+
+async function main(){
+const credential = new ClientCertificateCredential(process.env.VINAY_TENANT_ID, process.env.VINAY_CLIENT_ID,process.env.VINAY_cERTIFICATEPATH)
+try{
+    let accessToken = await credential.getToken("https://*.cacheinfra.windows.net:10225/appid/.default")
+    console.log("access Token",accessToken);
+
+    const client = createClient({username: "walmartserviceprincipal", password: accessToken.token, url: `redis://walmartserviceprincipal:${accessToken.token}@${process.env.VINAY_HOSTNAME}:6379` })
+    client.on('error', (err) => console.log('Redis Client Error', err));
+    //'redis://alice:foobared@awesome.redis.server:6380'
+    await client.connect();
+
+    await client.set('key', 'value');
+    // const value = await client.get('key');
+    // console.log("value", value);
+}
+catch(e){
+      console.log("error during get token -");
+  console.log(e);
+}
+
+}
+
+main().catch((err) => {
+    console.log("error code: ", err.code);
+    console.log("error message: ", err.message);
+    console.log("error stack: ", err.stack);
+  });
+  
